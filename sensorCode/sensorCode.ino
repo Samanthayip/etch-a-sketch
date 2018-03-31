f2fd4d78ccf28175dd27f9869912d9b08028f1cc
Han Jing <hanqijing@gmail.com>
2018-03-31 02:40:42 -0400

    clean things up

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,5 +1,4 @@
 #define SENSORPINA A0 // x axis
- //TODO: define other sensor inputs
 
 unsigned long targetTime=0;
 const unsigned long interval=500; //TODO: How fast should we read
@@ -20,11 +19,10 @@ int prevValX = 0;
 int prevValY = 0;
 int fsrVal = 0;
 
-
 int colorChange = 0;
 
 void setup(){
-  // TODO: begin the serial connection with a baudrate of 115200
+  // begin the serial connection with a baudrate of 115200
   pinMode(potPin, INPUT);
   pinMode(rotPin, INPUT);
   pinMode(buttonPin, INPUT);
@@ -33,63 +31,39 @@ void setup(){
   Serial.begin(115200);
 }
 void loop(){
-  //digitalWrite(ledPin, HIGH);
-    btnState = digitalRead(buttonPin);
-    prVal = analogRead(potPin);
-    pwWval = (255./1023.) * prVal; // not used
-    String x = String(prVal);
-    rrVal = analogRead(rotPin);
-    rwWval = (255./1023.) * rrVal; // not used
-    String y = String(rrVal);
-
+  btnState = digitalRead(buttonPin);
+  prVal = analogRead(potPin);
+  pwWval = (255./1023.) * prVal; // not used
+  String x = String(prVal);
+  rrVal = analogRead(rotPin);
+  rwWval = (255./1023.) * rrVal; // not used
+  String y = String(rrVal);
     
-    fsrVal = analogRead(fsr);
-    String fsrS = String(fsrVal);
+  fsrVal = analogRead(fsr);
+  String fsrS = String(fsrVal);
+
   if(millis()>=targetTime){
+
     targetTime= millis()+interval;
     if (btnState == HIGH) {
     } else {
-        Serial.println("rst"); // button is pressed
+        Serial.println("rst"); // send reset when button is pressed
     }
   
-    //Serial.println("fsr: " + fsrS);
     if (fsrVal > 3 and colorChange == 0) {
       colorChange = 1;
       Serial.println("color");
     }
-//    readValue = analogRead(potPin);
-//    writeValue = (255./1023.) * readValue;
-    //Serial.println(analogRead(SENSORPINA));
-    //Serial.println(prVal);
 
-    //Serial.println(rrVal);
     if ((abs(rrVal- prevValX) >= 50 )  or (abs(prVal- prevValY)>=50)) {
        digitalWrite(ledPin, HIGH);
     } else {
        digitalWrite(ledPin, LOW);
     }
     Serial.println(x + "," + y);
-//    Serial.println("rotPin", rrVal);
-//    analogWrite(ledPin, pwWval);
-    //Serial.println(analogRead(prVal));
-
-
-    //Serial.println(analogRead(rrVal));
-
-  //readValue = analogRead(potPin);
-  //writeValue = (255./1023.) * readValue;
-  //analogWrite(lightPin, writeValue);
-     //TODO: Add other sensor read outs
-     //TODO: convert values into a string https://www.arduino.cc/en/Tutorial/StringConstructors
-     //TODO: combine them into a string that can be understood by server.js
-     //TODO: send the string over serial
-
-
-        prevValX = rrVal;
-        prevValY = prVal;
+    prevValX = rrVal;
+    prevValY = prVal;
   }
-  // TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
-  // TODO: write the reset message(see server.js) to the serial port
 
 }
 
